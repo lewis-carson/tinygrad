2bc941b452297fa837a6e1b08e8eea9be1be7bc5
Lewis Carson <lcarson679@wallacehigh.net>
2023-06-01 20:55:21 +0100

    Basic dataset loading + skeleton of training loop

--- examples/mlperf/model_train.py
+++ examples/mlperf/model_train.py
@@ -6,7 +6,49 @@ def train_resnet():
   pass
 
 def train_retinanet():
-  # TODO: Retinanet
+  from pycocotools.coco import COCO
+  from datasets.openimages import openimages, iterate
+  from models.retinanet import RetinaNet
+  from models.resnet import ResNeXt50_32X4D
+  import numpy as np
+
+  coco = COCO(openimages())
+  model = RetinaNet(ResNeXt50_32X4D())
+  from tinygrad.jit import TinyJit
+
+
+  # TODO: replace this with proper normalization
+  input_mean = Tensor([0.485, 0.456, 0.406]).reshape(1, -1, 1, 1)
+  input_std = Tensor([0.229, 0.224, 0.225]).reshape(1, -1, 1, 1)
+  def input_fixup(x):
+    x = x.permute([0,3,1,2]) / 255.0
+    x -= input_mean
+    x /= input_std
+    return x
+
+  mdlrun = TinyJit(lambda x: model(input_fixup(x)).realize())
+
+  # TODO: make this proper batch size
+  bs = 8
+  epochs = 1
+
+  # TODO: finish training loop for RetinaNet
+  for epoch in range(epochs):
+    for x, targets in iterate(coco, bs):
+      print(x.shape)
+      dat = Tensor(x.astype(np.float32))
+
+      if dat.shape[0] == bs:
+        out = mdlrun(dat).numpy()
+      else:
+        mdlrun.jit_cache = None
+        out =  model(input_fixup(dat)).numpy()
+
+      predictions = model.postprocess_detections(out, input_size=dat.shape[1:3], orig_image_sizes=[t["image_size"] for t in targets])
+      
+      # TODO: loss function, optimizer, etc.
+      print(predictions.shape)
+
   pass
 
 def train_unet3d():
